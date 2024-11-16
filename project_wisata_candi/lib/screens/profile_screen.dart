f0726146c6795be3abcb5fdd62a43dd0631a1631
JordiWillyam <jordiwillyamadikusuma_2226240138@mhs.mdp.ac.id>
2024-11-16 17:01:03 +0700

    Update: vid 3 , menit 17

---
+++ project_wisata_candi/lib/screens/profile_screen.dart
@@ -0,0 +1,33 @@
+//Template default Stateful Widget
+
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            color: Colors.deepPurple,
+          ),
+          Column(
+            children: [],
+          )
+        ],
+      ),
+    );
+  }
+}
+// Template defaultnya samapai di sini
+
