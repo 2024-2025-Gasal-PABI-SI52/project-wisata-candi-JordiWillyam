95860f1aa73345f2cf32174c7df404ad0dec121d
JordiWillyam <jordiwillyamadikusuma_2226240138@mhs.mdp.ac.id>
2024-11-21 01:35:34 +0700

    Update : vid 3 , min 34.10

--- project_wisata_candi/lib/screens/profile_screen.dart
+++ project_wisata_candi/lib/screens/profile_screen.dart
@@ -12,17 +12,56 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    // Variable yang dibutuhkan
+    bool isSignIn = true;
+    String fullname = "";
+    String email = "";
+    int favoriteCandiCount = 0;
+
     return Scaffold(
       body: Stack(
+        alignment: Alignment.bottomRight,
         children: [
           Container(
             height: 200,
             width: double.infinity,
             color: Colors.deepPurple,
           ),
-          Column(
-            children: [],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                // Bagian Profile Header
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 150),
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.deepPurple,
+                              width: 2,
+                            ),
+                            shape: BoxShape.circle,
+                          ),
+                          child: CircleAvatar(
+                            radius: 50,
+                            backgroundImage: AssetImage("images/placeholder_image.png"),
+                          ),
+                    ),
+                    if (isSignIn) IconButton(onPressed: (){}, icon: Icon(Icons.camera_alt, color: Colors.deepPurple[50])),
+                    ],
+                    ),
+                  ),
+                ),
+                // Bagian profile info
+            
+                // Bagian profile Action
+            
+              ],
+            ),
           )
         ],
       ),
