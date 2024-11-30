aa8ea52ff28629e465611c1e45a17cb9d6093beb
JordiWillyam <jordiwillyamadikusuma_2226240138@mhs.mdp.ac.id>
2024-12-01 01:28:04 +0700

    update: vid 4 done

--- project_wisata_candi/lib/main.dart
+++ project_wisata_candi/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:project_wisata_candi/data/candi_data.dart';
 import 'package:project_wisata_candi/screens/detail_screen.dart';
 import 'package:project_wisata_candi/screens/profile_screen.dart';
 import 'package:project_wisata_candi/screens/signin_screen.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -15,7 +16,15 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: "Wisata Candi",
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.deepPurple),
+          titleTextStyle: TextStyle(color: Colors.deepPurple, 
+          fontSize: 20, fontWeight: FontWeight.bold),  
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+          primary: Colors.deepPurple,
+          surface: Colors.deepPurple[50],
+        ),
         useMaterial3: true,
       ),
       // home: DetailScreen(
--- project_wisata_candi/lib/screens/signin_screen.dart
+++ project_wisata_candi/lib/screens/signin_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 
-class SignInScreen extends StatelessWidget {
-  const SignInScreen({super.key});
+class SignInScreen extends StatefulWidget {
+  SignInScreen({super.key});
 
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
   // Deklarasi variabel
   final TextEditingController _usernameController = TextEditingController();
+
   final TextEditingController _passwordController = TextEditingController();
+
   String _errorText = '';
+
   bool _isSignIn = false;
+
   bool _obscurePassword = true;
 
   @override
@@ -18,72 +27,85 @@ class SignInScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Sign In'),
       ),
+
       // Body
       body: Center(
-        child: Form(
-            child: Column(
-          // Atur mainAxisAlignment dan crossAxisAlignment
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // buat TextFormField untuk nama pengguna
-            TextFormField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: "Nama Pengguna",
-                hintText: "Masukkan nama pengguna Anda",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            // buat TextFormField untuk kata sandi
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: "Kata Sandi",
-                hintText: "Masukkan kata sandi",
-                border: const OutlineInputBorder(),
-                errorText: _errorText.isNotEmpty ? _errorText : null,
-                suffixIcon: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    _obscurePassword ? Icons.visibility_off : Icons.visibility,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+                child: Column(
+              // Atur mainAxisAlignment dan crossAxisAlignment
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // buat TextFormField untuk nama pengguna
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    labelText: "Nama Pengguna",
+                    hintText: "Masukkan nama pengguna Anda",
+                    border: OutlineInputBorder(),
                   ),
                 ),
-              ),
-              obscureText: _obscurePassword,
-            ),
-            // buat ElevatedButton untuk sign in
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Sign In'),
-            ),
-            //Pasang textButton untuk sign up
-            const SizedBox(
-              height: 10,
-            ),
-            //   TextButton(onPressed: () {},
-            //   child: const Text('Belum punya akun? Daftar di sini.')
-            // ),
-            RichText(
-              text: TextSpan(
-                text: "Belum punya akun?",
-                style: TextStyle(color: Colors.deepPurple, fontSize: 16),
-                children: [
-                  TextSpan(
-                    text: " Daftar di sini.",
-                    style: const TextStyle(color: Colors.blue, fontSize: 16),
-                    decoration: TextDecoration.underline,
+                // buat TextFormField untuk kata sandi
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: "Kata Sandi",
+                    hintText: "Masukkan kata sandi",
+                    border: const OutlineInputBorder(),
+                    errorText: _errorText.isNotEmpty ? _errorText : null,
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+                      icon: Icon(
+                        _obscurePassword ? Icons.visibility_off : Icons.visibility,
+                      ),
+                    ),
+                  ),
+                  obscureText: _obscurePassword,
                 ),
-                recognizer: TapGestureRecognizer()
-                __onTap: () {},
+                // buat ElevatedButton untuk sign in
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Sign In'),
+                ),
+                //Pasang textButton untuk sign up
+                const SizedBox(
+                  height: 10,
+                ),
+                //   TextButton(onPressed: () {},
+                //   child: const Text('Belum punya akun? Daftar di sini.')
+                // ),
+                RichText(
+                  text: TextSpan(
+                    text: "Belum punya akun?",
+                    style: TextStyle(color: Colors.deepPurple, fontSize: 16),
+                    children: [
+                      TextSpan(
+                        text: " Daftar di sini.",
+                        style: const TextStyle(
+                          color: Colors.blue, 
+                          fontSize: 16,
+                          decoration: TextDecoration.underline,),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {},
+                      ),
+                    ],
+                  ),
+                )
               ],
-            ))
-          ],
-        )),
+            )),
+          ),
+        ),
       ),
     );
   }
